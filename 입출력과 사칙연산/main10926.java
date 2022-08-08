49f2c82349ff73e3d69f79c34b867b37de2210f0
GodSamble <osgneb8359@naver.com>
2022-08-08 20:58:08 +0900

    gogo

---
+++ 입출력과 사칙연산/main10926.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class main10926{
+
+    public static void main(String[] args){
+	
+	Scanner in = new Scanner(System.in);
+	
+	String a=in.next();
+	
+    System.out.println(a+"??!");
+    }
+}
