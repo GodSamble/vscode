8edb14b830f05b5be3f5ba3d685252a6cba465bc
GodSamble <osgneb8359@naver.com>
2022-08-11 01:25:34 +0900

    dya3

--- 입출력과 사칙연산/main10926.java
+++ 입출력과 사칙연산/main10926.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-class main10926{
+public class main10926{
 
     public static void main(String[] args){
 	
-	Scanner in = new Scanner(System.in);
+	Scanner scanner= new Scanner(System.in);
 	
-	String a=in.next();
+	String a=scanner.next();
+    scanner.close();
 	
     System.out.println(a+"??!");
     }
