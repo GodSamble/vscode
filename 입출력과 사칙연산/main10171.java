8f39ed4d199f19526c55fa78a33f494070aa04df
GodSamble <osgneb8359@naver.com>
2022-08-10 23:56:17 +0900

    day4

---
+++ 입출력과 사칙연산/main10171.java
@@ -0,0 +1,8 @@
+public class main10171{
+	public static void main(String[] args){
+	System.out.println("\\    /\\");
+	System.out.println(" )  ( ')");
+	System.out.println("(  /  )");
+	System.out.println(" \\(__)|");
+	}
+}
