d4af69ef7a501b4db687f567b253f9d3d4bfedf9
GodSamble <osgneb8359@naver.com>
2022-08-17 23:43:57 +0900

    day11

--- 백준 알고리즘/1차원 배열/main10818.java
+++ 백준 알고리즘/1차원 배열/main10818.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 public class main10818 {
+    /**
+     * @param args
+     */
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         
