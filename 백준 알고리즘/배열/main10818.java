244fa8152dd882153bc6f36225f98a7577aee4f1
GodSamble <osgneb8359@naver.com>
2022-08-23 22:40:21 +0900

    day16

--- 백준 알고리즘/배열/main10818.java
+++ 백준 알고리즘/배열/main10818.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 public class main10818 {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         
         int min = 1000000;
         int max = -1000000;
 
-        int N=scanner.nextInt();
+        int N=sc.nextInt();
         int[] score= new int[N];
 
         for(int i=0; i<N; i++){
-            score[i]=scanner.nextInt();
+            score[i]=sc.nextInt();
             if(max<score[i]){max=score[i];}
             if(min>score[i]){min=score[i];}
         }
-        scanner.close();
+        sc.close();
         System.out.println(min + " " + max);
     }    
 }
+//구글링 시  다양한 방법이 있으니
 //다양한 방법을 이용해서 메모리랑 작동시간단축을 위한 시도 해볼 것!
