57a4b4b5dbb8c551a1ef10e23cc6c26315e19852
GodSamble <osgneb8359@naver.com>
2022-09-04 22:58:55 +0900

    day9/4

--- 백준 알고리즘/입출력과 사칙연산/mainbreak정석예제.java
+++ 백준 알고리즘/입출력과 사칙연산/mainbreak정석예제.java
@@ -1,7 +1,7 @@
-public class mainbreak정석예제 {
+/*public class mainbreak정석예제 {
     public static void main(String[] args){
 
-Loop1:for(int i=2; i<=9; i++){
+Loop1: for(int i=2; i<=9; i++){
             for(int j=1; j<=9; j++){
             
             if(j==5)
@@ -19,4 +19,4 @@ Loop1:for(int i=2; i<=9; i++){
     
 }
 //자바의정석 178p 내용이며 구간 별 상이한거 확인 차, 해보기.
-//자바의정석 180p 프로그램 예제 하나 있는데 그걸 보면 '반복문/ if문/ break문, continuew문'이 적절하게 사용된 걸 볼 수 있다.
+//자바의정석 180p 프로그램 예제 하나 있는데 그걸 보면 '반복문/ if문/ break문, continuew문'이 적절하게 사용된 걸 볼 수 있다.*/
